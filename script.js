b3db7b447e5a7ce9ecb0b192d02338b72af11743
levantoann <lvtoan.19it4@vku.udn.vn>
2023-03-29 18:20:36 +0700

    Init page

---
+++ script.js
@@ -0,0 +1,14 @@
+VANTA.BIRDS({
+	el: "#vanta",
+	mouseControls:true,
+	touchControls:true,
+	gyroControls:true,
+	minHeight:600.00,
+	minWeight:200.00,
+	scale:1.00,
+	scaleMobile:1.00,
+	quantity:2.00,
+	backgroundAlpha:0.0,
+	algiment:77.00,
+})
+AOS.init();
